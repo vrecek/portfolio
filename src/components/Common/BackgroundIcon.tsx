b72c9a2c51e3aece5e1afcda5ff7df6a6a4d514c
vrecek <oktawiusz2002@gmail.com>
2024-02-20 18:55:28 +0100

    dist

--- src/components/Common/BackgroundIcon.tsx
+++
@@ -1,9 +0,0 @@
-const BackgroundIcon = ({ icon }: { icon: JSX.Element }) => {
-   return (
-      <span className="background-icon">
-         { icon }
-      </span>
-   )
-}
-
-export default BackgroundIcon
--- src/data/MAIL_ADDRESS.tsx
+++
@@ -1,3 +0,0 @@
-const MAIL_ADDRESS: string = 'vrecek-ffci@outlook.com'
-
-export default MAIL_ADDRESS
