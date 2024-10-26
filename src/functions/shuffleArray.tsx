e2371c61e5344d7a1228584cd9c9ea478a52eb96
vrecek <vrecek@proton.me>
2024-10-27 00:04:08 +0200

    dist

--- src/functions/shuffleArray.tsx
+++
@@ -1,8 +0,0 @@
-const shuffleArray = <T,>(array: T[]): T[] => {
-   return array
-      .map( value => ({ value, sort: Math.random() }) )
-      .sort( (a, b) => a.sort - b.sort )
-      .map( ({ value }) => value )
-}
-
-export default shuffleArray
